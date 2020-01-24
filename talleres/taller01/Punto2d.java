82980f7188a6e0ee362787d6c59daa6b01009430
jpmontoyag <36914298+jpmontoyag@users.noreply.github.com>
2020-01-24 10:47:31 -0500

    Add files via upload

---
+++ talleres/taller01/Punto2d.java
@@ -0,0 +1,55 @@
+import java.lang.Math;
+
+class Punto {
+
+private double x, y;
+
+    public Punto(double x, double y) {
+        this.x=x;
+        this.y=y;
+    }
+
+    public double getX() {
+      return x;
+    }
+
+    public double getY() {
+      return y;
+
+    }
+
+
+    public double radioPolar() {
+      double radio = Math.sqrt(Math.pow(getX(), 2)+Math.pow(getY(), 2));
+      System.out.println("Radio: "+radio+" cm");
+      return radio;
+    }
+
+    public double anguloPolar() {
+      double angulo = Math.atan(y/x);
+      System.out.println("Angulo: "+angulo+"°");
+      return angulo;
+    }
+
+
+
+    public double distanciaEuclidiana(Punto otro) {
+      double radio = Math.sqrt(Math.pow(otro.getX(), 2)+Math.pow(otro.getY(), 2));
+      System.out.println("Distancia ("+getX()+","+getY()+")-("+otro.getX()+","+otro.getY()+"): \n"+radio+" cm");
+      return radio;
+    }
+
+}
+
+class Main {
+
+  static Punto obj = new Punto(15, 69);
+  static Punto obj2 = new Punto(30, 120);
+
+  public static void main(String[] args) {
+
+    obj.radioPolar();
+    obj.anguloPolar();
+    obj.distanciaEuclidiana(obj2);
+  }
+}
